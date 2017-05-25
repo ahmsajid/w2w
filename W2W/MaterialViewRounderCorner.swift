99f449c0b0c1b7cd4ed0c7c7368dbc95e87b9ea1
A H M Masfiqur Rahman Sajid <sajid@As-MacBook-Pro.local>
2017-05-25 18:19:42 -0400

    Created TableView

--- W2W/MaterialViewRounderCorner.swift
+++ W2W/MaterialViewRounderCorner.swift
@@ -1,20 +1,24 @@
 //
-//  MaterialView.swift
+//  MaterialViewRounderCorner.swift
 //  W2W
 //
-//  Created by A H M Masfiqur Rahman Sajid on 5/22/17.
+//  Created by A H M Masfiqur Rahman Sajid on 5/25/17.
 //  Copyright © 2017 A H M Masfiqur Rahman Sajid. All rights reserved.
 //
 
 import UIKit
 
-class MaterialView: UIView {
-
+class MaterialViewRounderCorner: UIView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        layer.cornerRadius = 2.0
         layer.shadowColor = UIColor(red: CGFloat(SHADOW_COLOR), green: CGFloat(SHADOW_COLOR), blue: CGFloat(SHADOW_COLOR), alpha: 0.5).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
     }
 }
+
+
+
---
+++ W2W/RegistrationVC.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationVC.swift
+//  W2W
+//
+//  Created by A H M Masfiqur Rahman Sajid on 5/25/17.
+//  Copyright © 2017 A H M Masfiqur Rahman Sajid. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
