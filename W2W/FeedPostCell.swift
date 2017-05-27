673cc7098664e1a535d0c41a2f2008e0acdc01ad
A H M Masfiqur Rahman Sajid <sajid@As-MacBook-Pro.local>
2017-05-27 02:35:07 -0400

    Sample data inserted into tableview

---
+++ W2W/FeedPostCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedPostCell.swift
+//  W2W
+//
+//  Created by A H M Masfiqur Rahman Sajid on 5/27/17.
+//  Copyright © 2017 A H M Masfiqur Rahman Sajid. All rights reserved.
+//
+
+import UIKit
+
+class FeedPostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
